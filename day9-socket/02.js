af2c9695dcb7e290b4e303e428011316fe297b9b
heyushuo <1007561607@qq.com>
2018-03-07 16:57:36 +0800

    chat

--- day9-socket/02.js
+++ day9-socket/02.js
@@ -4,7 +4,7 @@ var fs = require("fs");
 var server = http.createServer(function(req,res){
 	if(req.url == "/"){
 		//显示首页
-		fs.readFile("./index.html",function(err,data){
+		fs.readFile("./huaban.html",function(err,data){
 			res.end(data);
 		});
 	}
@@ -15,9 +15,8 @@ var io = require('socket.io')(server);
 //监听连接事件
 io.on("connection",function(socket){
 	console.log("1个客户端连接了");
-	socket.on("tiwen",function(msg){
-		console.log("本服务器得到了一个提问" + msg);
-		socket.emit("huida","吃了");
+	socket.on("hua",function(msg){
+		io.emit("huida",msg);
 	});
 });
 
